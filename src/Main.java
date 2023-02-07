ed6b4c07361c65ecb7bf8a4b0cfd2695ada3cda1
ruslanTash <ruslan--t@yandex.ru>
2023-02-07 23:43:22 +0400

    Задачи выполнены

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import HW_3_3.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car myCar = new Car("Lada", "Granta", 2015, "Россия", "Жёлтый", 180,
+                5, "седан", 1.6, "x451po77", "МКПП 5", false, new Car.Key(false, false));
+
+        System.out.println(myCar.toString());
     }
 }
